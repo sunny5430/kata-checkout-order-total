4c3ccf3ba53df79c33beaae2b29a7e6d7987a76c
sunny5430 <tnsunnysun@gmail.com>
2021-05-05 15:33:48 -0400

    add create_special method, update cal_total method

--- order.py
+++ order.py
@@ -9,12 +9,11 @@ class Order:
         self.itemList = {}
         self.basket = {}
         self.markdown = {}
+        self.specials = {}
     
     def create_item(self, itemname, unit_type, unit_price):
         item = Item(itemname, unit_type, unit_price)
         self.itemList[itemname] = item
-        # self.itemPrice[itemname] = unit_price
-        # self.itemUnit[itemname] = unit_type
     
     def scan(self, itemname, weight=0):
         if itemname not in self.itemList:
@@ -38,15 +37,42 @@ class Order:
         total = 0
         for itemname, quantity in self.basket.items():
             item = self.itemList[itemname]
+            finalPrice = item.unit_price
+            
             if itemname in self.markdown:
                 finalPrice = item.unit_price - self.markdown[itemname]
+
+            if itemname in self.specials:
+                total += self.cal_total_with_special(itemname, quantity, finalPrice)
             else:
-                finalPrice = item.unit_price
             
-            # print(f"add {itemname}, {finalPrice}, {quantity}")
-            total += finalPrice * quantity
+                # print(f"add {itemname}, {finalPrice}, {quantity}")
+                total += finalPrice * quantity
         
         return total
+    
+    def cal_total_with_special(self, itemname, quantity, finalPrice):
+        condition, discount_rate, limit = self.specials[itemname]
+        total = 0
+
+        if quantity < condition:
+            total += finalPrice * quantity
+            return total
+            
+        if not limit or quantity < limit:
+            remaining = quantity % condition
+            discount_quantity = quantity - remaining
+        else: # quantity >= limit:
+            remaining = quantity % limit
+            discount_quantity = limit
+        
+        total += remaining * finalPrice
+        total += discount_quantity * finalPrice * discount_rate
+        
+        return total
+
+    
+    
     
     def remove(self, itemname, quantity):
         if itemname not in self.itemList or itemname not in self.basket:
@@ -63,9 +89,13 @@ class Order:
 
         print(self.basket)
 
+    
     def create_markdown(self, itemname, priceoff):
         if priceoff < 0:
             raise ValueError("Priceoff should be greater than 0")
+
+        if itemname not in self.itemList:
+            raise KeyError("Item not exists.")
 
         item = self.itemList[itemname]
         if priceoff > item.unit_price:
@@ -74,3 +104,11 @@ class Order:
         self.markdown[itemname] = priceoff
             
 
+    def create_special(self, itemname, buyM, getN, limit=None):
+        if itemname not in self.itemList:
+            raise KeyError("Item not exists.")
+        
+        condition = buyM + getN
+        discount_rate = getN / (buyM + getN)
+
+        self.specials[itemname] = [condition, discount_rate, limit]
--- test_order.py
+++ test_order.py
@@ -7,6 +7,9 @@ def order():
     order = Order()
     order.create_item("A", "weight", 3)
     order.create_item("B", "unit", 5)
+
+    order.scan("A", 2)
+    order.scan("B")
     return order
 
 
@@ -15,19 +18,13 @@ def test_createItem():
     order.create_item("A", "weight", 3)
 
 def test_scanItem(order):
-    order.scan("A", 2)
-    order.scan("B")
     with pytest.raises(KeyError):
         order.scan("ABC", 3)
 
 def test_calTotal(order):
-    order.scan("A", 2)
-    order.scan("B")
     assert order.cal_total() == 11
 
 def test_removeItem(order):
-    order.scan("A", 2)
-    order.scan("B")
     order.remove("A", 0.5)
     assert order.basket["A"] == 1.5
     with pytest.raises(ValueError):
@@ -43,11 +40,65 @@ def test_createMarkdown(order):
         order.create_markdown("B", -1)
     with pytest.raises(ValueError):
         order.create_markdown("B", 6)
+    with pytest.raises(KeyError):
+        order.create_markdown("C", 3)
 
-def test_calTotalWithMarkdown(order):
+def test_calTotal_WithMarkdown(order):
     order.create_markdown("A", 1)
-    order.scan("A", 2)
-    order.scan("B")
     assert order.cal_total() == 9
     order.remove("A", 0.5)
     assert order.cal_total() == 8
+
+def test_createSpecial(order):
+    with pytest.raises(KeyError):
+        order.create_special("C", 1, 1, 6)
+    
+    order.create_item("C", "unit", 10)
+    order.create_special("C", 1, 1, 6)
+
+
+# possible cases
+    # does not meet condition
+    # meet condition
+        # no limit
+        # less or equal to limit
+        # greater than limit
+
+def test_calTotal_WithSpecial(order):
+    
+    order.create_special("B", 10, 1)
+    assert order.cal_total() == 11
+    
+    order.create_item("C", "unit", 10)
+    order.create_special("C", 1, 1)
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    assert order.cal_total() == 31
+
+def test_calTotal_WithSpecial_limit(order):
+    order.create_item("C", "unit", 10)
+    order.create_special("C", 1, 1, 4)
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    assert order.cal_total() == 41
+
+    order.remove("C", 1)
+    assert order.cal_total() == 31
+
+def test_calTotal_WithMarkdown_WithSpecial_limit(order):
+    order.create_item("C", "unit", 10)
+    order.create_markdown("C", 9)
+    order.create_special("C", 1, 1, 4)
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    order.scan("C")
+    assert order.cal_total() == 14
+
+    order.remove("C", 1)
+    assert order.cal_total() == 13
